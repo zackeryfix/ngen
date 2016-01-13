d92c72e6190e22c8153a58bd517b1b48944c6ecf
Zackery Fix <zackeryfix@gmail.com>
2016-01-12 22:09:08 -0500

    Added UnitTest.Reflection project and testing workspace

---
+++ develop/now/UnitTest.Ngen.Reflection/Group.class_AssemblyInfo.cpp
@@ -0,0 +1,50 @@
+/*  _______    ________
+    \      \  /  _____/  ____   ___
+    /   |   \/   \  ____/ __ \ /   \
+   /    |    \    \_\  \  ___/|   | \
+   \____|__  /\______  /\___  >___| /
+           \/        \/     \/    \/
+The MIT License (MIT)
+
+COPYRIGHT (C) 2016 RAZORWARE, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include "_External.hpp"
+
+using namespace Ngen;
+using namespace Ngen::Diagnostic;
+
+t_testgroup(class_Assembly);
+
+t_begin_test(class_AssemblyInfo, AssemblyReferenceGetType) [] (TestResult& result) {
+   Assembly* assembly = Assembly::Reference(const_mirror(E"ngencore"), const_mirror(E"Ngen"));
+
+   auto object_type = assembly->GetType(const_mirror(E"Ngen.Object"));
+   object o = object_type->CreateInstance();
+
+   object ou = o.Invoke(const_mirror(E"Ngen:unknown%UnknownThis()%const"));
+
+   // when you are finished with the assembly reference, you need to dereference it
+   // since we only reference the assembly one time, dereferencing will release the System.rti library from memory
+   Assembly::Dereference(const_mirror(E"Ngen"));
+}
+t_end_test
+
+#endif
